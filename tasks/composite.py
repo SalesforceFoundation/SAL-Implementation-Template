4ad37581026790b0823ede9806ce252980606c69
James Estevez <jestevez@salesforce.com>
2021-02-10 09:34:19 -0800

    Use the built-in version

--- tasks/composite.py
+++ tasks/composite.py
@@ -2,32 +2,50 @@ import json
 import random
 import re
 import string
-import sys
 from pathlib import Path
 
 from cumulusci.cli.ui import CliTable
 from cumulusci.core.exceptions import SalesforceException
-from cumulusci.core.utils import process_bool_arg
+from cumulusci.core.utils import process_bool_arg, process_list_arg
 from cumulusci.tasks.salesforce import BaseSalesforceApiTask
 from cumulusci.utils import inject_namespace
 
-ENDPOINT = "composite"
 API_ROLLBACK_MESSAGE = "The transaction was rolled back since another operation in the same transaction failed."
 API_INVALID_REF_MESSAGE = "Invalid reference specified."
 
 
 class CompositeApi(BaseSalesforceApiTask):
-    """
-    Wrapper for Composite REST API calls.
+    task_docs = """
+This task is a wrapper for Composite REST API calls. Given a list of JSON files
+(one request body per file), POST each and process the returned composite
+result. Files are processed in the order given by the ``data_files`` option.
 
-    Replaces "%%%USERID%%%" and namespace tokens with the running user's
-    ID and project namespace, respectively. Replaces any usernames that include
-    the "connected.edu" domain with a random TLD.
+In addition, this task will process the request body and replace namespace
+(``%%%NAMESPACE%%%``) and user ID (``%%%USERID%%%``) tokens. To avoid username
+collisions, use the ``randomize_username`` option to replace the top-level
+domains in any ``Username`` field with a random string.
+
+When the top-level ``allOrNone`` property for the request is set to true a
+SalesforceException is raised if an error is returned for any subrequest,
+otherwise partial successes will not raise an exception.
+
+Example Task Definition
+-----------------------
+
+.. code-block::  yaml
+
+  tasks:
+      example_composite_request:
+          class_path: cumulusci.tasks.salesforce.composite.CompositeApi
+          options:
+             data_files:
+                 - "datasets/composite/users.json"
+                 - "datasets/composite/setup_objects.json"
     """
 
     task_options = {
         "data_files": {
-            "description": "A list of paths to the json files to POST.",
+            "description": "A list of paths, where each path is a JSON file containing a composite request body.",
             "required": True,
         },
         "managed": {
@@ -38,46 +56,55 @@ class CompositeApi(BaseSalesforceApiTask):
             "description": "If True, replaces namespace tokens with the namespace prefix.",
             "default": False,
         },
+        "randomize_username": {
+            "description": "If True, randomize the TLD for any 'Username' fields.",
+            "default": False,
+        },
     }
 
+    def _init_options(self, kwargs):
+        super()._init_options(kwargs)
+        self.data_files = process_list_arg(self.options.get("data_files") or [])
+
     def _run_task(self):
-        self.user_id = self._get_user_id()
-        for data_file_path in self.options["data_files"]:
+        for data_file_path in self.data_files:
             self.logger.info(f"Processing {data_file_path}")
             self._composite_request(data_file_path)
 
-    def _get_user_id(self):
-        result = self.sf.query(
-            f"SELECT Id FROM User WHERE UserName='{self.org_config.username}'"
-        )
-        return result["records"][0]["Id"]
-
     def _composite_request(self, data_file_path):
         request_body = self._process_json(Path(data_file_path).read_text())
-        self.is_all_or_none = json.loads(request_body)["allOrNone"]
-        client = self._init_api()
-        result = client.restful("composite", method="POST", data=request_body)
+        self.is_all_or_none = json.loads(request_body).get("allOrNone", False)
+        result = self.sf.restful("composite", method="POST", data=request_body)
         self._process_response(result)
 
     def _process_json(self, body):
         """Replace namespace tokens and randomize username domains."""
-        body = body.replace("%%%USERID%%%", self.user_id)
+        user_id = self.org_config.user_id
+        body = body.replace("%%%USERID%%%", user_id)
+
+        namespace = self.project_config.project__package__namespace
+        if "managed" in self.options:
+            managed = process_bool_arg(self.options["managed"])
+        else:
+            managed = (
+                bool(namespace) and namespace in self.org_config.installed_packages
+            )
+
         _, body = inject_namespace(
             "composite",
             body,
-            namespace=self.project_config.project__package__namespace,
-            managed=process_bool_arg( self.options.get("managed", False) ),
+            namespace=namespace,
+            managed=managed,
             namespaced_org=self.options.get("namespaced", self.org_config.namespaced),
         )
 
-        random_tld = "".join(random.choices(string.ascii_lowercase, k=4))
+        if self.options.get("randomize_username", True):
+            random_tld = "".join(random.choices(string.ascii_lowercase, k=4))
+            body = re.sub(
+                r'("Username": .[\w-]+@[\w-]+\.)+[\w-]+', fr"\1{random_tld}", body
+            )
 
-        def process_username_domain(match):
-            return match.group(0).replace("connected.edu", f"connected.{random_tld}")
-
-        processed_body = re.sub(r'"Username":.*', process_username_domain, body)
-
-        return processed_body
+        return body
 
     def _process_response(self, result):
         """Handle the compositeResponse and raise an exception if failed."""
@@ -86,31 +113,31 @@ class CompositeApi(BaseSalesforceApiTask):
 
         all_success = all([self._http_ok(code) for code in status_codes])
         if self.is_all_or_none and not all_success:
-            self._format_exception_message(subrequests)
+            self._log_exception_message(subrequests)
             raise SalesforceException(json.dumps(subrequests, indent=2))
         else:
-            self._format_success_message(subrequests)
+            self._log_success_message(subrequests)
 
     def _http_ok(self, status_code):
         return status_code >= 200 and status_code < 300
 
-    def _format_exception_message(self, subrequests):
+    def _log_exception_message(self, subrequests):
         table_data = [["ReferenceId", "Message"]]
         table_data.extend(
             [sub["referenceId"], body["message"]]
             for sub in subrequests
             for body in sub["body"]
-            if body["message"] != API_ROLLBACK_MESSAGE
+            if body.get("message") != API_ROLLBACK_MESSAGE
         )
         table = CliTable(table_data, wrap_cols=["Message"])
         self.logger.error("The request failed with the following message(s):\n\n")
-        table.echo()
+        self.logger.error("\n" + str(table))
 
-    def _format_success_message(self, subrequests):
+    def _log_success_message(self, subrequests):
         table_data = [["ReferenceId", "Success"]]
         table_data.extend(
             [sub["referenceId"], self._http_ok(sub["httpStatusCode"])]
             for sub in subrequests
         )
         table = CliTable(table_data, bool_cols=["Success"], title="Subrequest Results")
-        table.echo()
+        self.logger.info("\n" + str(table))
